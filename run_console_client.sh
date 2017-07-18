f44a65a71e9b0d41413f650eea21a085160deaae
glecorve <gwenole.lecorve@irisa.fr>
2017-07-18 11:46:06 +0200

    Update run_console_client.sh

--- run_console_client.sh
+++ run_console_client.sh
@@ -3,6 +3,6 @@
 # ./run_console_client.sh <client_url> <server_url>
 #
 
-CLASS_PATH=./build/classes
+CLASS_PATH=./bin
 
 java -Djava.security.policy=./security.policy -Djava.rmi.server.codebase=file:${CLASS_PATH} -cp ${CLASS_PATH} enshare.client.ConsoleClient $1 $2
