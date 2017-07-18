58fddd442bbe3e8e0eff74256000684aeeb317eb
glecorve <gwenole.lecorve@irisa.fr>
2017-07-18 11:46:23 +0200

    Update run_gui_client.sh

--- run_gui_client.sh
+++ run_gui_client.sh
@@ -3,6 +3,5 @@
 # ./run_client.sh <client_url> <server_url>
 #
 
-CLASS_PATH=./build/classes
-
+CLASS_PATH=./bin
 java -Djava.security.policy=./security.policy -Djava.rmi.server.codebase=file:${CLASS_PATH} -cp ${CLASS_PATH} enshare.client.GuiClient $1 $2
